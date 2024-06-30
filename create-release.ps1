abc10a832fc801de927b4db723f5d44fd44ff58f
daybr3ak <me@email.com>
2024-06-30 17:47:53 +0200

    basic release script

---
+++ create-release.ps1
@@ -0,0 +1,15 @@
+
+dotnet build --configuration Release
+rm -Force -Recurse bin/tmp/FapiQolPlugin
+mkdir bin/tmp/FapiQolPlugin  -erroraction silentlycontinue
+cp bin/Release/net472/FapiQolPlugin.dll bin/tmp/FapiQolPlugin
+cp bin/Release/net472/FapiQolPlugin.pdb bin/tmp/FapiQolPlugin
+cp readme.md bin/tmp/FapiQolPlugin
+
+$compress = @{
+  Path = "bin/tmp/FapiQolPlugin"
+  CompressionLevel = "Fastest"
+  DestinationPath = "bin\FapiQolPlugin-x64-win-1.0.0.zip"
+  Force = $true
+}
+Compress-Archive  @compress
